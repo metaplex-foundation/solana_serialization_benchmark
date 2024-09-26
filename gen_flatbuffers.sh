33147058059760066a8ffc9aa609b892cca943a8
Austbot <me@austbot.com>
2024-09-25 19:41:04 -0500

    rm temp

--- gen_flatbuffers.sh
+++ gen_flatbuffers.sh
@@ -3,3 +3,4 @@ flatc -I $(pwd)/programs/flatbuffers_schema/ --rust --gen-object-api --gen-mutab
 for file in ./crates/fb-types/src/*; do
     sed -e '/^#\[non_exhaustive\]/d' "$file" > temp_file && mv temp_file "$file"
 done
+rm ./crates/fb-types/src/temp_file
